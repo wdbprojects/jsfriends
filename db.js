afc50c0e728e9daf75188448cb674cc082ec57ea
Ronaldo Ortiz <ronyortizop@gmail.com>
2022-01-15 13:43:15 -0400

    fixed env variables

--- db.js
+++ db.js
@@ -2,16 +2,14 @@ const dotenv = require("dotenv");
 dotenv.config();
 const { MongoClient } = require("mongodb");
 
-const client = new MongoClient(
-  "mongodb+srv://webdevbro:t3l3cast3r@cluster0.mnxty.mongodb.net/jsfriends?retryWrites=true&w=majority",
-);
+const client = new MongoClient(process.env.CONNECTION_STRING);
 
 async function start() {
   await client.connect();
   module.exports = client;
   const app = require("./app");
   app.listen(process.env.PORT || 5000, () => {
-    console.log("Database connected on port 5000");
+    console.log(`Database connected on port ${process.env.PORT}`);
   });
 }
 
